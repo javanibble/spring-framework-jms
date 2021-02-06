adb21c7bc7eaa460f9f000c9b1b57328d28920d4
javanibble <javanibble@gmail.com>
2021-02-06 15:30:24 +0100

    Spring JMS: Messaging Example 02

---
+++ spring-jms-messaging-example-02/src/main/java/com/javanibble/spring/jms/springjmsmessagingexample02/SpringJmsMessagingExample02Application.java
@@ -0,0 +1,13 @@
+package com.javanibble.spring.jms.springjmsmessagingexample02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsMessagingExample02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsMessagingExample02Application.class, args);
+	}
+
+}
